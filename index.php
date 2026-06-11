6eb1ef4af8a729ea4e5b206f557f432c979fd2cf
Juan <juan275512@gmail.com>
2026-06-10 21:28:23 -0600

    leccion 5

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once "./vistas/vista_superior.php"?>
 
+<div class = "card-list"></div>
 
-<h1>Inicio</h1>
 
 
 <?php require_once "./vistas/vista_inferior.php"?>
--- vistas/vista_inferior.php
+++ vistas/vista_inferior.php
@@ -1,4 +1,6 @@
 <
+
 <script src="./script/hamburger-icon.js"></script>
+<script src="./script/cards.js"></script>
     </body>
 </html>
